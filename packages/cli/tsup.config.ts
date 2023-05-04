a1ba30a85daa2ccf9c4a44923129abfb0a6a4b0e
Joël Charles <joel.charles91@gmail.com>
2023-05-04 15:40:09 +0200

    chore(core): bundle core instead of cli

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -1,21 +1,10 @@
 import { defineConfig } from "@batijs/tsup";
 import esbuildBundleAllPlugin from "./esbuild-bundle-all";
-import esbuildFixEsqueryExportsPlugin from "./esbuild-fix-esquery-exports";
 
 export default defineConfig({
   entry: ["index.ts"],
   dts: true,
   outDir: "./dist",
-  esbuildPlugins: [esbuildBundleAllPlugin, esbuildFixEsqueryExportsPlugin],
-  noExternal: ["espree"],
-  shims: true,
+  esbuildPlugins: [esbuildBundleAllPlugin],
   platform: "node",
-  banner: {
-    js: `import { fileURLToPath as topFileURLToPath } from 'url';
-import { createRequire as topLevelCreateRequire } from 'module';
-const require = topLevelCreateRequire(import.meta.url);
-const __requireResolve = require.resolve;
-require.resolve = (x) => x === 'espree' ? undefined : __requireResolve(x);
-`,
-  },
 });
--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "@batijs/tsup";
+import esbuildFixEsqueryExportsPlugin from "./esbuild-fix-esquery-exports.js";
 
 export default defineConfig([
   {
@@ -8,5 +9,16 @@ export default defineConfig([
     bundle: true,
     outDir: "./dist",
     external: [],
+    esbuildPlugins: [esbuildFixEsqueryExportsPlugin],
+    shims: true,
+    platform: "node",
+    banner: {
+      js: `import { fileURLToPath as topFileURLToPath } from 'url';
+import { createRequire as topLevelCreateRequire } from 'module';
+const require = topLevelCreateRequire(import.meta.url);
+const __requireResolve = require.resolve;
+require.resolve = (x) => x === 'espree' ? undefined : __requireResolve(x);
+`,
+    },
   },
 ]);
