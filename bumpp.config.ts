48765a3b9f2c80559565a8828c4f6718e80f7795
Joël Charles <joel.charles91@gmail.com>
2023-06-29 22:40:02 +0200

    chore: better bumpp script

---
+++ bumpp.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "bumpp";
+
+export default defineConfig({
+  files: ["package.json", "packages/*/package.json", "packages/boilerplates/*/package.json"],
+  release: "patch",
+  commit: true,
+  push: true,
+  tag: true,
+});
