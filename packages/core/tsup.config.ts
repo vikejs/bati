6123d679d02d88eccacc4311f488c646b68ae3fa
Joël Charles <joel.charles91@gmail.com>
2023-09-04 11:46:41 +0200

    chore: replace rollup by tsup

---
+++ packages/core/tsup.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "tsup";
+import type { Plugin } from "esbuild";
+import { readFile } from "node:fs/promises";
+
+// Note: this is for putout because esbuild can't properly treeshake the
+// code, and is not aware that we do not use those dependencies.
+const putoutFixPlugin: Plugin = {
+  name: "putout-fix-plugin",
+  setup(build) {
+    build.onLoad({ filter: /engine-loader\/lib\/load\/load\.js$/ }, async (args) => {
+      const source = await readFile(args.path, "utf8");
+      const contents = source
+        .replace("createRequire(require.resolve(PUTOUT_YARN_PNP))", "require('putout')")
+        .replace("createRequire(require.resolve('putout'))", "require('putout')");
+      return { contents, loader: "default" };
+    });
+  },
+};
+
+export default defineConfig({
+  entry: ["./src/index.ts"],
+  platform: "node",
+  format: "esm",
+  target: "es2020",
+  outDir: "./dist",
+  dts: true,
+  bundle: true,
+  minify: true,
+  clean: true,
+  external: ["acorn-stage3", "hermes-parser", "tenko"],
+  esbuildPlugins: [putoutFixPlugin],
+  banner: {
+    js: `import { createRequire } from 'module';const require = createRequire(import.meta.url);`,
+  },
+});
