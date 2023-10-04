7e1ed520486640d8495cebb08644d997ea4fc4b5
Joël Charles <joel.charles91@gmail.com>
2023-10-04 20:54:21 +0200

    test:

--- packages/tests/src/index.ts
+++ packages/tests/src/index.ts
@@ -93,7 +93,7 @@ async function createTurboConfig(context: GlobalContext) {
         lint: {},
       },
       remoteCache: {
-        signature: true,
+        signature: false,
       },
     }),
     "utf-8",
