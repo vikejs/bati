ef52c786bffc4489baf939d597c97b04e136c012
Joël Charles <joel.charles91@gmail.com>
2023-10-12 18:22:05 +0200

    chore: update gif

---
+++ update-gif.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+tempD=$(mktemp -d)
+
+cd $tempD
+
+# Then run the following commands
+# pnpm --loglevel=error create @batijs/app --solid --tailwindcss --telefunc --eslint
+# cd my-app
+# pnpm install
+# pnpm run dev
+terminalizer record $scriptDir/doc/demo -k -c $scriptDir/terminalizer-config.yml
+
+# Then some manual edit is necessary
+
+cd -
+
+rm -fr $tempD
