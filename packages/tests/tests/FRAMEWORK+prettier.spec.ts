398786457592519b2b47413572f2d7ea6ff9bb1d
Joël Charles <joel.charles91@gmail.com>
2023-10-26 14:44:51 +0200

    test: prettier

---
+++ packages/tests/tests/FRAMEWORK+prettier.spec.ts
@@ -0,0 +1,11 @@
+import { describeBati } from "@batijs/tests-utils";
+
+export const matrix = [["solid", "react", "vue"], "prettier", "eslint"];
+
+await describeBati(({ test, expect, fetch }) => {
+  test("home", async () => {
+    const res = await fetch("/");
+    expect(res.status).toBe(200);
+    expect(await res.text()).not.toContain('{"is404":true}');
+  });
+});
