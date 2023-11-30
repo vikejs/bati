7d3425bd60ac35843300332cf75897eeb2de5ba2
Joël Charles <joel.charles91@gmail.com>
2023-11-30 11:27:07 +0100

    fix: only activate jsxRuntime: "classic" for a specific combination

--- boilerplates/react/files/$vite.config.ts.ts
+++ boilerplates/react/files/$vite.config.ts.ts
@@ -3,22 +3,23 @@ import { addVitePlugin, loadAsMagicast, type TransformerProps } from "@batijs/co
 export default async function getViteConfig(props: TransformerProps) {
   const mod = await loadAsMagicast(props);
 
-  const options = props.meta.BATI.has("vercel")
+  const vikeOptions = props.meta.BATI.has("vercel")
     ? {
         prerender: true,
       }
     : {};
+  // See https://github.com/batijs/bati/pull/124
+  const reactOptions = props.meta.BATI.has("vercel") && props.meta.BATI.has("hattip") ? { jsxRuntime: "classic" } : {};
 
   addVitePlugin(mod, {
     from: "@vitejs/plugin-react",
     constructor: "react",
-    // see https://github.com/vitejs/vite/discussions/5803#discussioncomment-5562200
-    options: {},
+    options: reactOptions,
   });
   addVitePlugin(mod, {
     from: "vike/plugin",
     constructor: "ssr",
-    options,
+    options: vikeOptions,
   });
 
   return mod.generate().code;
