cc7d4fe98bc4c42fd1e01348d92c8e2dee73b002
Kenzo-Wada <used.acormhib@gmail.com>
2023-12-12 01:53:22 +0900

    chore: update component by using Tooltip component

--- website/components/Presets.tsx
+++ website/components/Presets.tsx
@@ -2,12 +2,13 @@ import type { CategoryLabels, Flags } from "@batijs/features";
 import { StoreContext } from "#components/Store.js";
 import clsx from "clsx";
 import { useContext } from "solid-js";
+import { Tooltip } from "./Tooltip";
 
-function Preset(props: { title: string; features: (Flags | CategoryLabels)[]; class?: string; disabled?: boolean; description?: string }) {
+function Preset(props: { title: string; features: (Flags | CategoryLabels)[]; class?: string; disabled?: boolean; description: string }) {
   const { selectPreset } = useContext(StoreContext);
 
   return (
-    <div class='tooltip tooltip-bottom' data-tip={props.description}>
+    <Tooltip tip={props.description}>
       <button
         type="button"
         disabled={props.disabled}
@@ -19,18 +20,18 @@ function Preset(props: { title: string; features: (Flags | CategoryLabels)[]; cl
       >
         {props.title}
       </button>
-    </div>
+    </Tooltip>
   );
 }
 
 export default function Presets() {
   return (
     <div class="w-full flex box-border gap-2">
-      <Preset title="Plain Vike" features={[]} description="Scaffold a simple app with Vike" />
-      <Preset title="Frontend" features={["Framework", "CSS", "Linter"]} description="Scaffold a frontend app with Solid and Tailwind CSS" />
-      <Preset title="Full-stack" features={["Framework", "RPC", "Auth", "Database", "CSS", "Server", "Linter"]} description="Scaffold a full-stack app with RPC (like server actions), Auth, Database and Server" />
-      <Preset title="Next.js" features={["react", "Auth", "RPC", "Server", "vercel", "Linter"]} description="Scaffold a Next.js like app with RPC (like server actions) and Vercel" />
-      <Preset title="E-commerce" features={["Framework"]} disabled={true} />
+      <Preset title="Plain Vike" features={[]} description="Simple app with Plain Vike" />
+      <Preset title="Frontend" features={["Framework", "CSS", "Linter"]} description="Frontend app with Solid and Tailwind CSS" />
+      <Preset title="Full-stack" features={["Framework", "RPC", "Auth", "Database", "CSS", "Server", "Linter"]} description="Full-stack app with RPC (like server actions), Auth, Database and Server" />
+      <Preset title="Next.js" features={["react", "Auth", "RPC", "Server", "vercel", "Linter"]} description="Next.js like app with RPC (like server actions) and Vercel" />
+      <Preset title="E-commerce" features={["Framework"]} disabled={true} description="" />
     </div>
   );
 }
