bebd74c9969f3751a333d78d37d4e0f7543cf5b7
Joël Charles <joel.charles91@gmail.com>
2024-01-17 13:21:44 +0100

    chore: utils for .env files manipulation

--- boilerplates/prisma/files/$.env.ts
+++ boilerplates/prisma/files/$.env.ts
@@ -1,25 +1,18 @@
-import { type TransformerProps } from "@batijs/core";
+import { appendToEnv, type TransformerProps } from "@batijs/core";
 
 export default async function getEnv(props: TransformerProps) {
-  let envContent = (await props.readfile?.()) ?? "";
-  if (envContent.endsWith("\n\n")) {
-    // do nothing
-  } else if (envContent.endsWith("\n")) {
-    envContent = envContent + "\n";
-  } else if (envContent) {
-    envContent = envContent + "\n\n";
-  }
+  const envContent = await props.readfile?.();
 
-  const prismaContent = `# Prisma
+  return appendToEnv(
+    envContent,
+    "DATABASE_URL",
+    "postgresql://johndoe:randompassword@localhost:5432/mydb?schema=public",
+    `Prisma
 
-# Environment variables declared in this file are automatically made available to Prisma.
-# See the documentation for more detail: https://pris.ly/d/prisma-schema#accessing-environment-variables-from-the-schema
+Environment variables declared in this file are automatically made available to Prisma.
+See the documentation for more detail: https://pris.ly/d/prisma-schema#accessing-environment-variables-from-the-schema
 
-# Prisma supports the native connection string format for PostgreSQL, MySQL, SQLite, SQL Server, MongoDB and CockroachDB.
-# See the documentation for all the connection string options: https://pris.ly/d/connection-strings
-
-DATABASE_URL="postgresql://johndoe:randompassword@localhost:5432/mydb?schema=public"
-`;
-
-  return envContent + prismaContent;
+Prisma supports the native connection string format for PostgreSQL, MySQL, SQLite, SQL Server, MongoDB and CockroachDB.
+See the documentation for all the connection string options: https://pris.ly/d/connection-strings`,
+  );
 }
---
+++ packages/core/src/utils/env.ts
@@ -0,0 +1,40 @@
+export function appendToEnv(envContent: string | undefined | null, key: string, value: unknown, comment?: string) {
+  envContent ??= "";
+  if (envContent.endsWith("\n\n")) {
+    // do nothing
+  } else if (envContent.endsWith("\n")) {
+    envContent = envContent + "\n";
+  } else if (envContent) {
+    envContent = envContent + "\n\n";
+  }
+
+  const prefixedComment = comment ? comment.replace(/^(.+)/gm, "# $1") + "\n" : "";
+  const newConf = `${key}=${formatValue(value)}\n`;
+
+  return envContent + prefixedComment + newConf;
+}
+
+function formatValue(value: unknown): string {
+  let strval = "";
+  switch (typeof value) {
+    case "string":
+      if (value) {
+        strval = JSON.stringify(value);
+      }
+      break;
+    case "boolean":
+    case "number":
+      strval = String(value);
+      break;
+    case "undefined":
+      strval = "";
+      break;
+    case "object":
+      if (value !== null) {
+        strval = JSON.stringify(value);
+      }
+      break;
+  }
+
+  return strval;
+}
