1fc80219a45a1273807d4da5b7de81c4e21743d0
Muhammad <phonzammizaki@gmail.com>
2024-02-18 01:12:50 +0700

    add firebase code to express-entry

--- boilerplates/express/files/express-entry.ts
+++ boilerplates/express/files/express-entry.ts
@@ -1,10 +1,14 @@
+import "dotenv/config";
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 import CredentialsProvider from "@auth/core/providers/credentials";
 import { appRouter } from "@batijs/trpc/trpc/server";
 import { createMiddleware } from "@hattip/adapter-node";
 import * as trpcExpress from "@trpc/server/adapters/express";
-import express from "express";
+import cookieParser from "cookie-parser";
+import express, { type Request } from "express";
+import { applicationDefault, deleteApp, getApp, getApps, initializeApp, type App } from "firebase-admin/app";
+import { getAuth, type UserRecord } from "firebase-admin/auth";
 import { telefunc } from "telefunc";
 import { VikeAuth } from "vike-authjs";
 import { renderPage } from "vike/server";
@@ -13,6 +17,30 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const isProduction = process.env.NODE_ENV === "production";
 const root = __dirname;
+
+if (BATI.has("firebase-auth")) {
+  declare module "express" {
+    interface Request {
+      user?: UserRecord | null;
+    }
+  }
+
+  let admin: App | undefined;
+
+  function initializeAdminApp() {
+    return initializeApp({
+      credential: applicationDefault(),
+    });
+  }
+
+  if (!getApps().length) {
+    admin = initializeAdminApp();
+  } else {
+    admin = getApp();
+    deleteApp(admin);
+    admin = initializeAdminApp();
+  }
+}
 
 startServer();
 
@@ -74,6 +102,52 @@ async function startServer() {
     );
   }
 
+  if (BATI.has("firebase-auth")) {
+    app.use(cookieParser());
+    app.use(async function (req: Request, _, next) {
+      const sessionCookie: string = req.cookies.__session || "";
+
+      try {
+        const auth = getAuth();
+        const decodedIdToken = await auth.verifySessionCookie(sessionCookie, true);
+        const user = await auth.getUser(decodedIdToken.sub);
+        req.user = user;
+      } catch (error) {
+        console.log("error verifySessionCookie :", error);
+        req.user = null;
+      }
+
+      next();
+    });
+
+    app.use(express.json()); // Parse & make HTTP request body available at `req.body`
+    app.post("/api/sessionLogin", (req: Request, res) => {
+      const idToken: string = req.body.idToken || "";
+
+      const expiresIn = 60 * 60 * 24 * 5 * 1000; // 5 days
+
+      getAuth()
+        .createSessionCookie(idToken, { expiresIn })
+        .then(
+          (sessionCookie) => {
+            // Set cookie policy for session cookie.
+            const options = { maxAge: expiresIn, httpOnly: true, secure: true };
+            res.cookie("__session", sessionCookie, options);
+            res.end(JSON.stringify({ status: "success" }));
+          },
+          (error) => {
+            console.log("createSessionCookie error :", error);
+            res.status(401).send("UNAUTHORIZED REQUEST!");
+          },
+        );
+    });
+
+    app.post("/api/sessionLogout", function (_, res) {
+      res.clearCookie("__session");
+      res.end();
+    });
+  }
+
   if (BATI.has("trpc")) {
     /**
      * tRPC route
@@ -127,8 +201,12 @@ async function startServer() {
    *
    * @link {@see https://vike.dev}
    **/
-  app.all("*", async (req, res, next) => {
-    const pageContextInit = { urlOriginal: req.originalUrl };
+  app.all("*", async (req: Request, res, next) => {
+    if (BATI.has("firebase-auth")) {
+      const pageContextInit = { urlOriginal: req.originalUrl, user: req.user };
+    } else {
+      const pageContextInit = { urlOriginal: req.originalUrl };
+    }
     const pageContext = await renderPage(pageContextInit);
     if (pageContext.httpResponse === null) return next();
 
