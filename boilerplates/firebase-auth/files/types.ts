5cf3d21c901e275d188cd97e6bed31966289be62
Muhammad <phonzammizaki@gmail.com>
2024-02-25 00:22:20 +0700

    minor

--- boilerplates/firebase-auth/files/types.ts
+++ boilerplates/firebase-auth/files/types.ts
@@ -3,14 +3,14 @@ import type { FirebaseApp } from "firebase/app";
 
 declare global {
   /*{ @if (it.BATI.has("express")) }*/
-  // eslint-disable-next-line @typescript-eslint/no-namespace
+  // eslint-disable-next-line
   namespace Express {
     interface Request {
       user?: UserRecord | null;
     }
   }
   /*{ /if }*/
-  // eslint-disable-next-line @typescript-eslint/no-namespace
+  // eslint-disable-next-line
   namespace Vike {
     interface PageContext {
       user?: UserRecord | null;
