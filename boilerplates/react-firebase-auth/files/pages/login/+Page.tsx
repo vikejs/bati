03b88accd0b1f361e78d628326462151142bdd1d
Muhammad <phonzammizaki@gmail.com>
2024-02-24 11:53:45 +0700

    minor react-firebase-auth

--- boilerplates/react-firebase-auth/files/pages/login/+Page.tsx
+++ boilerplates/react-firebase-auth/files/pages/login/+Page.tsx
@@ -9,49 +9,52 @@ export default Page
 
 function Page() {
     const [error, setError] = useState("")
-    const auth = getAuth()
 
     useEffect(() => {
         const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(getAuth())
-
-        // Please read firebaseui docs at https://github.com/firebase/firebaseui-web
-        ui.start("#firebaseui-auth-container", {
-            callbacks: {
-                signInSuccessWithAuthResult(authResult: UserCredential) {
-                    authResult.user.getIdToken().then((idToken) => {
-                        fetch("/api/sessionLogin", {
-                            method: "POST",
-                            body: JSON.stringify({ idToken }),
-                            headers: {
-                                "Content-Type": "application/json",
-                            },
-                        }).then(async (response) => {
-                            if (response.ok) {
-                                await reload()
-                            } else {
-                                setError(response.statusText)
-                            }
+        if (!error) {
+            // Please read firebaseui docs at https://github.com/firebase/firebaseui-web
+            ui.start("#firebaseui-auth-container", {
+                callbacks: {
+                    signInSuccessWithAuthResult(authResult: UserCredential) {
+                        authResult.user.getIdToken().then((idToken) => {
+                            fetch("/api/sessionLogin", {
+                                method: "POST",
+                                body: JSON.stringify({ idToken }),
+                                headers: {
+                                    "Content-Type": "application/json",
+                                },
+                            }).then(async (response) => {
+                                if (response.ok) {
+                                    await reload()
+                                } else {
+                                    setError(response.statusText)
+                                }
+                            })
                         })
-                    })
-                    // Don't redirect after firebase client successfully sign-in, let vike handle the rest.
-                    return false
-                }
-            },
-            signInFlow: "popup",
-            signInOptions: [
-                // Disable "Email enumeration protection" to be able to login with registered email address
-                // https://console.firebase.google.com/u/1/project/{project-id}/authentication/settings
-                firebase.auth.EmailAuthProvider.PROVIDER_ID,
-                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-            ],
-        })
-    }, [auth.currentUser])
+                        // Don't redirect after firebase client successfully sign-in, let vike handle the rest.
+                        return false
+                    }
+                },
+                signInFlow: "popup",
+                signInOptions: [
+                    // Disable "Email enumeration protection" to be able to login with registered email address
+                    // https://console.firebase.google.com/u/1/project/{project-id}/authentication/settings
+                    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                ],
+            })
+        }
+    }, [error])
 
     return (
         <>
             <div id="firebaseui-auth-container"></div>
             {error && (
-                <div style={{ color: "red" }}>There is an error occured : {error}</div>
+                <>
+                    <div style={{ color: "red" }}>There is an error occured : {error}</div>
+                    <button onClick={() => setError("")}>Try Again</button>
+                </>
             )}
         </>
     )
