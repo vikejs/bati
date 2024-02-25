117c13a9a62b54f775a6ad01e94aae915259887e
Muhammad <phonzammizaki@gmail.com>
2024-02-26 00:35:34 +0700

    minor

--- boilerplates/firebase-auth/files/$.env.ts
+++ boilerplates/firebase-auth/files/$.env.ts
@@ -8,12 +8,12 @@ export default async function getEnv(props: TransformerProps) {
     "GOOGLE_APPLICATION_CREDENTIALS",
     "firebase/service-account.json",
     `Your web app's Firebase configuration:
- VITE_FIREBASE_API_KEY="value",
- VITE_FIREBASE_AUTH_DOMAIN="value",
- VITE_FIREBASE_PROJECT_ID="value",
- VITE_FIREBASE_STORAGE_BUCKET="value",
- VITE_FIREBASE_MESSAGING_SENDER_ID="value",
- VITE_FIREBASE_APP_ID="value",
+ VITE_FIREBASE_API_KEY="",
+ VITE_FIREBASE_AUTH_DOMAIN="",
+ VITE_FIREBASE_PROJECT_ID="",
+ VITE_FIREBASE_STORAGE_BUCKET="",
+ VITE_FIREBASE_MESSAGING_SENDER_ID="",
+ VITE_FIREBASE_APP_ID="",
 
  Location of Your Firebase service account (use for firebase-admin).
  Download the file from https://console.firebase.google.com/u/0/project/{firebase-project-id}/settings/serviceaccounts/adminsdk`,
