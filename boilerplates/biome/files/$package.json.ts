7c10237694f369607c7534ee95d858b669d45e7a
“Kenzo-Wada” <used.acormhib@gmail.com>
2024-03-12 01:52:59 +0900

    feat: add biome

---
+++ boilerplates/biome/files/$package.json.ts
@@ -0,0 +1,20 @@
+import { addDependency, loadAsJson, setScripts, type TransformerProps } from "@batijs/core";
+
+export default async function getPackageJson(props: TransformerProps) {
+  const packageJson = await loadAsJson(props);
+
+  setScripts(packageJson, {
+    lint: {
+      value: "biome lint --apply ./",
+      precedence: 0
+    },
+    format: {
+      value: "biome format --write ./",
+      precedence: 0
+    }
+  });
+
+  return addDependency(packageJson, await import("../package.json", { assert: { type: "json" } }), {
+    devDependencies: ["@biomejs/biome"],
+  });
+}
