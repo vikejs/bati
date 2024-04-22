380e694ad178b3ffd17809b6acec4cdb6a8d2dcc
Joël Charles <joel.charles91@gmail.com>
2024-04-22 12:36:27 +0200

    chore: update default es target to 2022

--- packages/features/tsup.config.ts
+++ packages/features/tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: { index: "./src/index.ts", rules: "./src/rules/index.ts" },
   platform: "neutral",
   format: "esm",
-  target: "es2021",
+  target: "es2022",
   bundle: true,
   clean: true,
   dts: true,
