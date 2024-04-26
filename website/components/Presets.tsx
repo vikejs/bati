9121211ae6171639a14b52cd8173f2981599c983
Joël Charles <joel.charles91@gmail.com>
2024-04-26 14:28:10 +0200

    chore: fix types

--- website/components/Presets.tsx
+++ website/components/Presets.tsx
@@ -44,12 +44,12 @@ export default function Presets() {
       <Preset title="Plain Vike" features={[]} description="Simple app with Plain Vike" />
       <Preset
         title="Frontend"
-        features={["Framework", "CSS", "Linter"]}
+        features={["UI Framework", "CSS", "Linter"]}
         description="Frontend app with a Framework and Tailwind CSS"
       />
       <Preset
         title="Full-stack"
-        features={["Framework", "RPC", "Auth", "Database", "CSS", "Server", "Linter"]}
+        features={["UI Framework", "RPC", "Auth", "Database", "CSS", "Server", "Linter"]}
         description="Full-stack app with RPC (like server actions), Auth, Database and Server"
       />
       <Preset
@@ -57,7 +57,7 @@ export default function Presets() {
         features={["react", "Auth", "RPC", "Server", "vercel", "Linter"]}
         description="Next.js like app with RPC (like server actions) and Vercel"
       />
-      <Preset title="E-commerce" features={["Framework"]} disabled={true} description="" />
+      <Preset title="E-commerce" features={["UI Framework"]} disabled={true} description="" />
     </div>
   );
 }
