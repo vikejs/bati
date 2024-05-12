8fa41aca7d1b47814f7837f0b18e8df72eb4c35f
Muhammad <phonzammizaki@gmail.com>
2024-05-12 21:32:37 +0700

    more fixed

--- boilerplates/react/files/pages/_error/+Page.tsx
+++ boilerplates/react/files/pages/_error/+Page.tsx
@@ -10,12 +10,11 @@ export default function Page() {
         <p>This page could not be found.</p>
       </>
     );
-  } else {
-    return (
-      <>
-        <h1>500 Internal Server Error</h1>
-        <p>Something went wrong.</p>
-      </>
-    );
   }
+  return (
+    <>
+      <h1>500 Internal Server Error</h1>
+      <p>Something went wrong.</p>
+    </>
+  );
 }
