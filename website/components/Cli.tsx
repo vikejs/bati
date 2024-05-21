8fd236cabba38e3519e9c19293653a23ba4147fc
Joël Charles <joel.charles91@gmail.com>
2024-05-21 14:58:34 +0200

    chore(website): adding yarn, bun and npm commands

---
+++ website/components/Cli.tsx
@@ -0,0 +1,42 @@
+import { copy } from "#components/Copy.js";
+import { flip } from "#components/Flip.js";
+import type { JSX } from "solid-js";
+
+// avoid removing import when trying to optimize them
+// https://github.com/solidjs/solid/discussions/845
+copy;
+flip;
+
+export function Cli(
+  props: {
+    tooltipText: string;
+  } & Pick<JSX.HTMLAttributes<unknown>, "onMouseEnter" | "onClick" | "children">,
+) {
+  return (
+    <kbd
+      role="tabpanel"
+      class="group join-item rounded-md cursor-pointer relative flex-1 justify-start pl-9 tooltip tooltip-primary text-left inline-flex tooltip-bottom kbd kbd-sm select-all flex-wrap leading-9 gap-2.5"
+      use:copy
+      onMouseEnter={props.onMouseEnter}
+      onClick={props.onClick}
+      data-tip={props.tooltipText}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="lucide lucide-terminal absolute top-2 left-2 opacity-40 h-5"
+      >
+        <polyline points="4 17 10 11 4 5" />
+        <line x1="12" x2="20" y1="19" y2="19" />
+      </svg>
+      {props.children}
+    </kbd>
+  );
+}
--- website/components/Widget.tsx
+++ website/components/Widget.tsx
@@ -1,17 +1,29 @@
-import { copy } from "#components/Copy.js";
+import { Cli } from "#components/Cli";
 import Description from "#components/Description.js";
 import Features from "#components/Features.js";
-import { flip } from "#components/Flip.js";
 import Messages from "#components/Messages.js";
 import Presets from "#components/Presets.js";
 import Stackblitz from "#components/Stackblitz";
 import { StoreContext } from "#components/Store.js";
-import { createMemo, createSignal, Show, useContext } from "solid-js";
+import { createMemo, createSignal, Show, useContext, type JSX } from "solid-js";
 
-// avoid removing import when trying to optimize them
-// https://github.com/solidjs/solid/discussions/845
-copy;
-flip;
+export function CliGroup(
+  props: {
+    tooltipText: string;
+    flags: string[];
+  } & Pick<JSX.HTMLAttributes<unknown>, "onMouseEnter" | "onClick" | "children">,
+) {
+  return (
+    <div role="tabpanel" class="tab-content w-full bg-base-100 border-base-300 rounded-md p-1.5">
+      <div class="sm:join flex flex-row w-full rounded-md">
+        <Cli onMouseEnter={props.onMouseEnter} onClick={props.onClick} tooltipText={props.tooltipText}>
+          {props.children}
+        </Cli>
+        <Stackblitz flags={props.flags} class="join-item hidden sm:flex" />
+      </div>
+    </div>
+  );
+}
 
 export function Widget(props: { theme?: string; widget: boolean }) {
   const { selectedFeaturesFlags, rules } = useContext(StoreContext);
@@ -21,7 +33,10 @@ export function Widget(props: { theme?: string; widget: boolean }) {
     return selectedFeaturesFlags().map((flag) => `--${flag}`);
   }
 
-  const words = createMemo(() => ["pnpm", "create", "@batijs/app", ...getFlags()]);
+  const pnpm = createMemo(() => ["pnpm", "create", "@batijs/app", ...getFlags()]);
+  const yarn = createMemo(() => ["yarn", "dlx", "@batijs/cli", ...getFlags()]);
+  const bun = createMemo(() => ["bun", "create", "@batijs/app", ...getFlags()]);
+  const npm = createMemo(() => ["npm", "create", "@batijs/app", ...getFlags()]);
 
   const handleMouseEnter = () => {
     setTooltipText("Copy to Clipboard");
@@ -42,32 +57,32 @@ export function Widget(props: { theme?: string; widget: boolean }) {
       <div class="mb-2 w-full">
         <Description />
       </div>
-      <div class="flex px-4 sm:join">
-        <kbd
-          class="cursor-pointer group join-item relative flex-1 justify-start pl-9 tooltip tooltip-primary text-left inline-flex tooltip-bottom kbd kbd-sm select-all flex-wrap rounded-md leading-9 gap-2.5"
-          use:copy
-          onMouseEnter={handleMouseEnter}
-          onClick={handleCopy}
-          data-tip={tooltipText()}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-terminal absolute top-2 left-2 opacity-40 h-5"
-          >
-            <polyline points="4 17 10 11 4 5" />
-            <line x1="12" x2="20" y1="19" y2="19" />
-          </svg>
-          {words().join(" ")}
-        </kbd>
-        <Stackblitz flags={getFlags()} class="join-item hidden sm:flex" />
+      <div class="flex">
+        <div role="tablist" class="tabs tabs-lifted tabs-sm flex-1">
+          <input type="radio" name="package_manager" role="tab" class="tab" aria-label="pnpm" checked />
+
+          <CliGroup onMouseEnter={handleMouseEnter} onClick={handleCopy} tooltipText={tooltipText()} flags={getFlags()}>
+            {pnpm().join(" ")}
+          </CliGroup>
+
+          <input type="radio" name="package_manager" role="tab" class="tab" aria-label="yarn" />
+
+          <CliGroup onMouseEnter={handleMouseEnter} onClick={handleCopy} tooltipText={tooltipText()} flags={getFlags()}>
+            {yarn().join(" ")}
+          </CliGroup>
+
+          <input type="radio" name="package_manager" role="tab" class="tab" aria-label="bun" />
+
+          <CliGroup onMouseEnter={handleMouseEnter} onClick={handleCopy} tooltipText={tooltipText()} flags={getFlags()}>
+            {bun().join(" ")}
+          </CliGroup>
+
+          <input type="radio" name="package_manager" role="tab" class="tab" aria-label="npm" />
+
+          <CliGroup onMouseEnter={handleMouseEnter} onClick={handleCopy} tooltipText={tooltipText()} flags={getFlags()}>
+            {npm().join(" ")}
+          </CliGroup>
+        </div>
       </div>
       <Show when={rules().size > 0}>
         <div class="flex flex-col gap-2 leading-6 rounded-md mt-4">
