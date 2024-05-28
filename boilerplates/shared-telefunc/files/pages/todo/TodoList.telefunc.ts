2b813ee0d486423f17bdcd0bd340e420ebd66e9d
Muhammad <phonzammizaki@gmail.com>
2024-05-28 22:49:47 +0700

    new-boilerplate: `shared-telefunc` (only use with drizzle for now)

---
+++ boilerplates/shared-telefunc/files/pages/todo/TodoList.telefunc.ts
@@ -0,0 +1,9 @@
+import { db } from "@batijs/drizzle/database/db";
+import { todoTable, type TodoInsert } from "@batijs/drizzle/database/schema";
+import type { RunResult } from "better-sqlite3";
+
+export async function onCreateTodo({ text }: TodoInsert) {
+  const result: RunResult = await db.insert(todoTable).values({ text });
+
+  return { result };
+}
