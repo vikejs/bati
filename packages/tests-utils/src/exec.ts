f5b8a99139c5994c0fbaf06291dae0c6c25d4fc4
Joël Charles <joel.charles91@gmail.com>
2024-05-29 00:23:26 +0200

    chore: use powershell on windows

--- packages/tests-utils/src/exec.ts
+++ packages/tests-utils/src/exec.ts
@@ -1,6 +1,12 @@
-import { $, type Options } from "zx";
+import { $, usePowerShell, type Options } from "zx";
+
+const isWin = process.platform === "win32";
 
 $.preferLocal = true;
+
+if (isWin) {
+  usePowerShell();
+}
 
 export function exec(file: string, args?: string[], options?: Partial<Options>) {
   const childProcess = $({
