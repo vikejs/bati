1ea27ce79a9e3eda93f38b122ac8e56ee0f35731
Joël Charles <joel.charles91@gmail.com>
2024-05-29 11:54:21 +0200

    chore: do not fail tests if no AUTH0 env var

--- packages/tests/tests/FRAMEWORK+SERVER+AUTH.dom.spec.ts
+++ packages/tests/tests/FRAMEWORK+SERVER+AUTH.dom.spec.ts
@@ -1,14 +1,15 @@
 import { describeBati, dom } from "@batijs/tests-utils";
 
-export const matrix = [
-  ["express", "h3", "hono", "fastify", "hattip"],
-  [...(process.env.TEST_AUTH0_CLIENT_ID ?? process.env.AUTH0_CLIENT_ID ? (["auth0"] as const) : [])],
-  "eslint",
-] as const;
+export const matrix = [["express", "h3", "hono", "fastify", "hattip"], ["auth0"], "eslint"] as const;
 
 await describeBati(({ testMatch, expect, context }) => {
   testMatch<typeof matrix>("auth/signin", {
     auth0: async () => {
+      // Skip test if those are not available
+      if (!process.env.TEST_AUTH0_CLIENT_ID || !process.env.AUTH0_CLIENT_ID) {
+        expect(1).toBe(1);
+        return;
+      }
       const browser = new dom.Browser();
       const page = browser.newPage();
 
