50bf9b9775c7f8c6883d7e03ad9d7adc99157b21
Muhammad <phonzammizaki@gmail.com>
2024-06-19 00:22:38 +0700

    add more JSDoc comment

--- boilerplates/ts-rest/files/ts-rest/client.ts
+++ boilerplates/ts-rest/files/ts-rest/client.ts
@@ -3,6 +3,12 @@ import { contract } from "./contract";
 
 const port = typeof location !== "undefined" ? location.port : "3000";
 
+/**
+ * ts-rest client
+ *
+ * This is the basic client, using fetch under the hood which is exported from @ts-rest/core
+ * @link {@see https://ts-rest.com/docs/core/fetch/}
+ **/
 export const client = initClient(contract, {
   // TODO: properly retrieve host
   baseUrl: `http://localhost:${port}`,
