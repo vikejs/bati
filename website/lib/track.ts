4de06ccc17baf781154a6e68ccaabc17ee9697b4
Joël Charles <joel.charles91@gmail.com>
2024-06-19 13:01:45 +0200

    chore: udpate tracking

--- website/lib/track.ts
+++ website/lib/track.ts
@@ -9,7 +9,7 @@ interface CopyEvent {
 }
 
 interface Zaraz {
-  track: <T extends Events>(event: T["name"], data: T["data"]) => Promise<void>;
+  track: <T extends Events>(event: T["name"], data: unknown) => Promise<void>;
 }
 
 declare global {
@@ -17,10 +17,37 @@ declare global {
   var zaraz: Zaraz | undefined;
 }
 
+function generateUUID() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0,
+      v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
 export function track<T extends Events>(event: T["name"], data: T["data"]) {
+  const interactionId = generateUUID();
   console.log({
+    interactionId,
     event,
     data,
   });
-  globalThis.zaraz?.track(event, data);
+  switch (event) {
+    case "copy": {
+      if (data.flags.length === 0) {
+        globalThis.zaraz?.track(event, {
+          flag: undefined,
+          package_manager: data.package_manager,
+          interaction_id: interactionId,
+        });
+      }
+      for (const flag of data.flags) {
+        globalThis.zaraz?.track(event, {
+          flag,
+          package_manager: data.package_manager,
+          interaction_id: interactionId,
+        });
+      }
+    }
+  }
 }
