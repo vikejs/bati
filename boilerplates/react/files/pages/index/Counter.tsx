2c94e5410879e0a29c6156c292a61722e2f72a6b
Muhammad <phonzammizaki@gmail.com>
2024-06-20 01:23:20 +0700

    add compiled css usage

--- boilerplates/react/files/pages/index/Counter.tsx
+++ boilerplates/react/files/pages/index/Counter.tsx
@@ -8,6 +8,18 @@ export function Counter() {
       type="button"
       //# BATI.has("tailwindcss")
       className="inline-block border border-black rounded bg-gray-200 px-2 py-1 text-xs font-medium uppercase leading-normal"
+      //# BATI.has("compiled-css")
+      css={{
+        display: "inline-block",
+        border: "1px solid black",
+        borderRadius: "0.25rem",
+        backgroundColor: "#e5e7eb",
+        padding: "4px 8px 4px 8px",
+        fontSize: "0.75rem",
+        fontWeight: "500",
+        textTransform: "uppercase",
+        lineHeight: "1.5",
+      }}
       onClick={() => setCount((count) => count + 1)}
     >
       Counter {count}
