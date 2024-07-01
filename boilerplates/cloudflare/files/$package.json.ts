bad5836449472d36d3357f32d4bb1bfb1bb0748a
Joël Charles <joel.charles91@gmail.com>
2024-07-01 15:52:55 +0200

    feat: Support Cloudflare Pages

---
+++ boilerplates/cloudflare/files/$package.json.ts
@@ -0,0 +1,13 @@
+import { addDependency, loadAsJson, type TransformerProps } from "@batijs/core";
+
+export default async function getPackageJson(props: TransformerProps) {
+  const packageJson = await loadAsJson(props);
+
+  return addDependency(packageJson, await import("../package.json").then((x) => x.default), {
+    devDependencies: ["wrangler"],
+    dependencies: [
+      "vike-cloudflare",
+      ...(props.meta.BATI.has("hattip") ? (["@hattip/adapter-cloudflare-workers"] as const) : []),
+    ],
+  });
+}
