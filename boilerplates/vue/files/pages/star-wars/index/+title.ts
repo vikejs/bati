bc84c45e551ed28b85337ac02542caa3ba2e3711
Joël Charles <joel.charles91@gmail.com>
2024-07-02 09:17:14 +0200

    fix(vue): +data structure

--- boilerplates/vue/files/pages/star-wars/index/+title.ts
+++ boilerplates/vue/files/pages/star-wars/index/+title.ts
@@ -2,6 +2,6 @@ import type { PageContext } from "vike/types";
 import type { Data } from "./+data.js";
 
 export function title(pageContext: PageContext<Data>) {
-  const movies = pageContext.data;
+  const movies = pageContext.data.data;
   return `${movies.length} Star Wars Movies`;
 }
