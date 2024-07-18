bad63b89bf7c814f847b1f4fb2856d09eb699779
Muhammad <phonzammizaki@gmail.com>
2024-07-18 20:41:55 +0700

    chore: deps

--- boilerplates/lucia-auth/files/$package.json.ts
+++ boilerplates/lucia-auth/files/$package.json.ts
@@ -4,8 +4,11 @@ export default async function getPackageJson(props: TransformerProps) {
   const packageJson = await loadAsJson(props);
 
   return addDependency(packageJson, await import("../package.json").then((x) => x.default), {
-    devDependencies: ["@types/better-sqlite3"],
+    devDependencies: ["@types/better-sqlite3", "@types/cookie"],
     dependencies: [
+      "arctic",
+      "cookie",
+      "lucia",
       ...(props.meta.BATI.has("drizzle")
         ? (["@lucia-auth/adapter-drizzle"] as const)
         : (["better-sqlite3", "@lucia-auth/adapter-sqlite"] as const)),
