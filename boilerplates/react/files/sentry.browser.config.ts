6cb70a7ff6e0dde3da7c5137b9f49c38529efd28
Andreas Heissenberger <andreas@heissenberger.at>
2024-08-29 09:22:00 +0200

    fix: remove react router, disable Sentry in DEV, disable SessionTracking in SSR

--- boilerplates/react/files/sentry.browser.config.ts
+++ boilerplates/react/files/sentry.browser.config.ts
@@ -1,32 +1,23 @@
 //# BATI.has("REMOVE-COMMENT") || "remove-comments-only"
 //# BATI include-if-imported
 
-//import { useEffect } from "react";
 import * as Sentry from "@sentry/react";
-//import { createRoutesFromChildren, matchRoutes, useLocation, useNavigationType } from "react-router-dom";
-Sentry.init({
-  dsn: import.meta.env.DEV ? "" : import.meta.env.PUBLIC_ENV__SENTRY_DSN,
-  environment: "production-frontend",
-  integrations: [
-    // See docs for support of different versions of variation of react router
-    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
-    // Sentry.reactRouterV6BrowserTracingIntegration({
-    //   useEffect,
-    //   useLocation,
-    //   useNavigationType,
-    //   createRoutesFromChildren,
-    //   matchRoutes,
-    // }),
-    Sentry.replayIntegration(),
-  ],
-  autoSessionTracking: globalThis?.window?.document ? true : false,
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for tracing.
-  tracesSampleRate: 1.0,
-  // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
-  tracePropagationTargets: [/^\//, /^https:\/\/yourserver\.io\/api/],
-  // Capture Replay for 10% of all sessions,
-  // plus for 100% of sessions with an error
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
+
+// eslint-disable-next-line
+import.meta.env.PROD === true &&
+  Sentry.init({
+    dsn: import.meta.env.PUBLIC_ENV__SENTRY_DSN,
+    environment: "production-frontend",
+    //enabled: import.meta.env.DEV ? false : true,
+    integrations: [Sentry.replayIntegration()],
+    autoSessionTracking: globalThis?.window?.document ? true : false, // disable autoSessionTracking in SSR
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for tracing.
+    tracesSampleRate: 1.0,
+    // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
+    tracePropagationTargets: [/^\//, /^https:\/\/yourserver\.io\/api/],
+    // Capture Replay for 10% of all sessions,
+    // plus for 100% of sessions with an error
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
