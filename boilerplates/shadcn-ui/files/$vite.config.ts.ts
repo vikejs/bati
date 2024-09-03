31b4b92127035527be44c3cfd68691edf9739802
Andreas Heissenberger <andreas@heissenberger.at>
2024-09-03 10:25:56 +0200

    fix: handle both types (array,object) of data structures in resolve.alias

--- boilerplates/shadcn-ui/files/$vite.config.ts.ts
+++ boilerplates/shadcn-ui/files/$vite.config.ts.ts
@@ -1,19 +1,53 @@
-import { loadAsMagicast, type TransformerProps, deepMergeObject } from "@batijs/core";
+import { loadAsMagicast, type TransformerProps } from "@batijs/core";
 
 export default async function getViteConfig(props: TransformerProps) {
   const mod = await loadAsMagicast(props);
 
-  // @ts-ignore
-  deepMergeObject(mod.exports.default.$args[0], {
-    resolve: {
-      alias: [
-        {
-          find: "@",
-          replacement: "{{PLACEHOLDER_ALIAS}}",
-        },
-      ],
-    },
-  });
+  const replacement = `new URL("./", import.meta.url).pathname`;
+  addResolveAlias(mod, "@", replacement);
 
-  return mod.generate().code.replace(`"{{PLACEHOLDER_ALIAS}}"`, `new URL("./", import.meta.url).pathname`);
+  // convert placeholder (type is string) to replacement (type is expression) to be evaluated in final code
+  return mod.generate().code.replace(`"{{PLACEHOLDER_ALIAS}}"`, replacement);
+}
+
+function addResolveAlias(mod, find: string, replacement: string) {
+  const alias = mod.exports.default.$args[0]?.resolve?.alias ?? {};
+  const warnMsg = `${yellow("WARN")}: new resolve.alias: \`"${find}":${replacement}\` replaced existing in vite.config.ts.`;
+  if (alias?.length === undefined) {
+    if (alias[find]) {
+      console.warn(warnMsg);
+    }
+    alias[find] = "{{PLACEHOLDER_ALIAS}}";
+  } else {
+    // type is array
+    const newAlias = {
+      find: find,
+      replacement: "{{PLACEHOLDER_ALIAS}}",
+    };
+    const index = alias.findIndex((a: { find: string }) => a?.find === find);
+    if (index >= 0) {
+      console.warn(warnMsg);
+      alias.splice(index, 1);
+    }
+    alias.push(newAlias);
+  }
+  if (!mod.exports.default.$args[0]?.resolve) mod.exports.default.$args[0].resolve = {};
+  mod.exports.default.$args[0].resolve.alias = alias;
+}
+
+function yellow(str: string) {
+  const { env = {}, argv = [], platform = "" } = typeof process === "undefined" ? {} : process;
+
+  const isDisabled = "NO_COLOR" in env || argv.includes("--no-color");
+  const isForced = "FORCE_COLOR" in env || argv.includes("--color");
+  const isWindows = platform === "win32";
+  const isDumbTerminal = env.TERM === "dumb";
+
+  const isCompatibleTerminal = env.TERM && !isDumbTerminal;
+
+  const isCI = "CI" in env && ("GITHUB_ACTIONS" in env || "GITLAB_CI" in env || "CIRCLECI" in env);
+
+  const isColorSupported = !isDisabled && (isForced || (isWindows && !isDumbTerminal) || isCompatibleTerminal || isCI);
+
+  return isColorSupported ? `\x1b[43m${str}\x1b[0m` : str;
 }
