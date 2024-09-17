c9d573064431c40a9d7552eec29d9bfd4d0398e4
Joël Charles <joel.charles91@gmail.com>
2024-09-17 21:26:48 +0200

    chore: lint

--- boilerplates/shared-no-db/files/database/todoItems.ts
+++ boilerplates/shared-no-db/files/database/todoItems.ts
@@ -1,17 +1,14 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 interface TodoItem {
   text: string;
 }
 
 const todosDefault = [{ text: "Buy milk" }, { text: "Buy strawberries" }];
 
-const database: { todos: TodoItem[] } =
-  // We create a in-memory database.
+const database =
+  // We create an in-memory database.
   // - We use globalThis so that the database isn't reset upon HMR.
   // - The database is reset when restarting the server, use a proper database (SQLite/PostgreSQL/...) if you want persistent data.
-  // biome-ignore lint:
-  ((globalThis as any).__database ??= { todos: todosDefault });
+  ((globalThis as unknown as { __database: { todos: TodoItem[] } }).__database ??= { todos: todosDefault });
 
 const { todos } = database;
 
