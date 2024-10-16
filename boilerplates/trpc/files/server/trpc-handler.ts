d6544be1f79b2b2292ade005f22bb897cf7130a6
Joël Charles <joel.charles91@gmail.com>
2024-10-16 10:32:25 +0200

    chore: upgrade deps

--- boilerplates/trpc/files/server/trpc-handler.ts
+++ boilerplates/trpc/files/server/trpc-handler.ts
@@ -2,6 +2,7 @@
 import { appRouter } from "../trpc/server";
 // TODO: stop using universal-middleware and directly integrate server middlewares instead. (Bati generates boilerplates that use universal-middleware https://github.com/magne4000/universal-middleware to make Bati's internal logic easier. This is temporary and will be removed soon.)
 import type { Get, UniversalHandler } from "@universal-middleware/core";
+import type { D1Database } from "@cloudflare/workers-types";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
 export const trpcHandler = ((endpoint) => (request, context, runtime) => {
