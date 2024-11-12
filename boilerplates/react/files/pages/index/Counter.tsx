7888f010cb413bd583c9c6f773c34e316aa885f6
Muhammad <phonzammizaki@gmail.com>
2024-11-12 23:10:24 +0700

    add usage with `React`

--- boilerplates/react/files/pages/index/Counter.tsx
+++ boilerplates/react/files/pages/index/Counter.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { css } from "../../styled-system/css";
 
 export function Counter() {
   const [count, setCount] = useState(0);
@@ -6,8 +7,22 @@ export function Counter() {
   return (
     <button
       type="button"
-      //# BATI.has("tailwindcss")
-      className="inline-block border border-black rounded bg-gray-200 px-2 py-1 text-xs font-medium uppercase leading-normal"
+      //# BATI.has("tailwindcss") || BATI.has("panda-css")
+      className={
+        BATI.has("tailwindcss")
+          ? "inline-block border border-black rounded bg-gray-200 px-2 py-1 text-xs font-medium uppercase leading-normal"
+          : css({
+              display: "inline-block",
+              border: "1px solid black",
+              rounded: "sm",
+              bg: "gray.200",
+              px: 1,
+              py: 0.5,
+              fontSize: 12,
+              fontWeight: 500,
+              lineHeight: "16px",
+            })
+      }
       //# BATI.has("compiled-css")
       css={{
         display: "inline-block",
--- boilerplates/react/files/pages/todo/TodoList.tsx
+++ boilerplates/react/files/pages/todo/TodoList.tsx
@@ -1,6 +1,7 @@
 import { onNewTodo } from "@batijs/telefunc/pages/todo/TodoList.telefunc";
 import { trpc } from "@batijs/trpc/trpc/client";
 import { client } from "@batijs/ts-rest/ts-rest/client";
+import { css } from "../../styled-system/css";
 import React, { useState } from "react";
 
 export function TodoList({ initialTodoItems }: { initialTodoItems: { text: string }[] }) {
@@ -48,8 +49,55 @@ export function TodoList({ initialTodoItems }: { initialTodoItems: { text: strin
             }
           }}
         >
-          <input type="text" onChange={(ev) => setNewTodo(ev.target.value)} value={newTodo} />{" "}
-          <button type="submit">Add to-do</button>
+          <input
+            type="text"
+            onChange={(ev) => setNewTodo(ev.target.value)}
+            value={newTodo}
+            //# BATI.has("tailwindcss") || BATI.has("panda-css")
+            className={
+              BATI.has("tailwindcss")
+                ? "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full sm:w-auto p-2 mr-1 mb-1"
+                : css({
+                    p: 2,
+                    bg: "gray.50",
+                    borderWidth: 1,
+                    borderColor: "gray.300",
+                    color: "gray.900",
+                    fontSize: "sm",
+                    rounded: "md",
+                    width: { base: "full", sm: "auto" },
+                    _focus: { ringColor: "teal.500", borderColor: "teal.500" },
+                    mr: 1,
+                    mb: 1,
+                  })
+            }
+          />
+          <button
+            type="submit"
+            //# BATI.has("tailwindcss") || BATI.has("panda-css")
+            className={
+              BATI.has("tailwindcss")
+                ? "text-white bg-blue-700 hover:bg-blue-800 focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto p-2"
+                : css({
+                    color: "white",
+                    bg: { base: "teal.700", _hover: "teal.800" },
+                    _focus: {
+                      ringWidth: 2,
+                      ringColor: "teal.300",
+                      outline: "1px solid transparent",
+                      outlineOffset: "1px",
+                    },
+                    cursor: "pointer",
+                    fontSize: "sm",
+                    fontWeight: 500,
+                    rounded: "md",
+                    width: { base: "full", sm: "auto" },
+                    p: 2,
+                  })
+            }
+          >
+            Add to-do
+          </button>
         </form>
       </div>
     </>
