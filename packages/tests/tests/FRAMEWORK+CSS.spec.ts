c2d0615b38a9616017137b2636a657e51eb64155
Muhammad <phonzammizaki@gmail.com>
2024-11-13 14:58:56 +0700

    e2e test

--- packages/tests/tests/FRAMEWORK+CSS.spec.ts
+++ packages/tests/tests/FRAMEWORK+CSS.spec.ts
@@ -1,7 +1,9 @@
 import { readFile } from "node:fs/promises";
 import { describeBati } from "@batijs/tests-utils";
+import { existsSync } from "node:fs";
+import path from "node:path";
 
-export const matrix = [["solid", "react", "vue"], ["tailwindcss", "daisyui"], "eslint"];
+export const matrix = [["solid", "react", "vue"], ["tailwindcss", "daisyui", "panda-css"], "eslint"];
 
 await describeBati(({ test, expect, fetch, context }) => {
   test("home", async () => {
@@ -13,5 +15,10 @@ await describeBati(({ test, expect, fetch, context }) => {
   test("daiysui", async () => {
     const content = await readFile("tailwind.config.ts", "utf-8");
     expect(content.includes("daisyui")).toBe(context.flags.includes("daisyui"));
+  });
+
+  test("panda-css", async () => {
+    expect(existsSync(path.join(process.cwd(), "panda.config.ts"))).toBe(true);
+    expect(existsSync(path.join(process.cwd(), "layouts", "panda.css"))).toBe(true);
   });
 });
