9869978d9dddb6e408878ec684bb871bcace5dd3
Joël Charles <joel.charles91@gmail.com>
2024-12-06 15:08:08 +0100

    doc: Complete Cloudflare D1 README

--- boilerplates/d1/files/$README.md.ts
+++ boilerplates/d1/files/$README.md.ts
@@ -8,19 +8,21 @@ export default async function getReadme(props: TransformerProps) {
 ## *Cloudflare D1*
 
 ### Setup
-Create a D1 database:
-
+Create a D1 database with the following command:
 \`\`\`sh
 wrangler d1 create <your-db-name>
 \`\`\`
+
+Then, copy the output to \`wrangler.toml\`.
+
+Finally, update the \`d1:migrate\` script (in \`package.json\`) to replace \`YOUR_DATABASE_NAME\`, and execute it.
 
 > [!NOTE]
 > For reference, a good database name is:
 > - Typically a combination of ASCII characters, shorter than 32 characters, and uses dashes (-) instead of spaces.
 > - Descriptive of the use-case and environment. For example, “staging-db-web” or “production-db-backend”.
 > - Only used for describing the database, and is not directly referenced in code.
-
-Then copy the output to \`wrangler.toml\``;
+`;
 
   content.addMarkdownFeature(todo, "sqlite");
 
