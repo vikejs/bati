37c0c8c2aecf0be8ddba77511123114c0a43fd11
Muhammad <phonzammizaki@gmail.com>
2025-04-29 21:04:14 +0700

    fix: update tailwindcss-vue integration

--- boilerplates/vue/files/pages/+Head.vue
+++ boilerplates/vue/files/pages/+Head.vue
@@ -2,6 +2,8 @@
 
 <template>
   <link rel="icon" :href="logoUrl" />
+  <!-- BATI.has("tailwindcss") -->
+  <link rel="stylesheet" :href="tailwindCss" />
 
   <!-- BATI.has("plausible.io") -->
   <!-- See https://plausible.io/docs/plausible-script -->
@@ -11,4 +13,6 @@
 
 <script setup lang="ts">
 import logoUrl from "../assets/logo.svg";
+//# BATI.has("tailwindcss")
+import * as tailwindCss from "@batijs/tailwindcss/layouts/tailwind.css";
 </script>
