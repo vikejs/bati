d35d60616151ae93b30119da38dbb8068dbf1819
Joël Charles <joel.charles91@gmail.com>
2025-05-19 11:24:45 +0200

    chore

--- packages/tests/src/common.ts
+++ packages/tests/src/common.ts
@@ -175,6 +175,7 @@ export async function createKnipConfig(projectDir: string, flags: string[], scri
   if (flags.includes("aws")) {
     entry.push("entry_aws_lambda.ts");
     entry.push("cdk/lib/vike-stack.ts");
+    entry.push("tests/aws_handler.spec.ts");
     ignoreDependencies.push("aws-cdk", "cdk", "esbuild", "npm-run-all2");
     ignore.push("cdk.out/**");
   }
