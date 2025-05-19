89e5370f3c0075dbe6fa62498cc8a01aa047c8e8
Joël Charles <joel.charles91@gmail.com>
2025-05-19 11:24:45 +0200

    chore

--- boilerplates/vue/files/pages/+onCreateApp.ts
+++ boilerplates/vue/files/pages/+onCreateApp.ts
@@ -1,15 +1,14 @@
 import type { OnCreateAppSync } from "vike-vue/types";
-import VueGtag from "vue-gtag";
+import { createGtag } from "vue-gtag";
 
 // BATI.has("google-analytics")
 export const onCreateApp: OnCreateAppSync = (pageContext): ReturnType<OnCreateAppSync> => {
   const { app } = pageContext;
 
   // See https://matteo-gabriele.gitbook.io/vue-gtag/
-  app.use(VueGtag, {
-    customResourceURL: "https://www.googletagmanager.com/gtag/js",
-    config: {
-      id: import.meta.env.PUBLIC_ENV__GOOGLE_ANALYTICS,
-    },
+  const gtag = createGtag({
+    tagId: import.meta.env.PUBLIC_ENV__GOOGLE_ANALYTICS,
   });
+
+  app.use(gtag);
 };
