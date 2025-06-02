6081de6eee9dc030b9a7313633e1fb20f1ee6d0a
Joël Charles <joel.charles91@gmail.com>
2025-06-02 15:31:16 +0200

    revert bunExists

--- packages/tests-utils/src/package-manager.ts
+++ packages/tests-utils/src/package-manager.ts
@@ -1,5 +1,4 @@
 import which from "which";
 
-const isWin = process.platform === "win32";
-export const bunExists = isWin ? false : which.sync("bun", { nothrow: true }) !== null;
+export const bunExists = which.sync("bun", { nothrow: true }) !== null;
 export const npmCli = bunExists && !process.env.FORCE_PNPM ? "bun" : "pnpm";
