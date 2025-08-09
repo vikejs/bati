88ac6483f1752024e88079666f6359f3a2741a7c
Joël Charles <joel.charles91@gmail.com>
2025-08-09 11:05:38 +0200

    chore: lint

--- packages/build/src/operations/merge-dts.ts
+++ packages/build/src/operations/merge-dts.ts
@@ -62,7 +62,7 @@ export function clearExports(code: string, meta: VikeMeta) {
     const index = code.indexOf("\nexport {};");
     const foundImport = code.match(/^import .* from /gm);
 
-    if (index === -1 && !foundImport) {
+    if (index !== -1 && !foundImport) {
       return (
         code.slice(0, index) +
         "\n// biome-ignore lint/complexity/noUselessEmptyExport: ensure that the file is considered as a module" +
