111d2e09bf90f236a4bbf256c8a5b87d01a50c03
Rom <git@brillout.com>
2025-09-16 22:17:41 +0200

    fix: improve error page (#567)

--- boilerplates/solid/files/pages/_error/+Page.tsx
+++ boilerplates/solid/files/pages/_error/+Page.tsx
@@ -8,12 +8,12 @@ export default function Page() {
       when={is404}
       fallback={
         <>
-          <h1>500 Internal Server Error</h1>
+          <h1>Internal Error</h1>
           <p>Something went wrong.</p>
         </>
       }
     >
-      <h1>404 Page Not Found</h1>
+      <h1>Page Not Found</h1>
       <p>This page could not be found.</p>
     </Show>
   );
