1505ab81190634e94413b4ddfbd0a312e638c4f2
Romuald Brillout <git@brillout.com>
2025-09-16 21:59:08 +0200

    minor refactor: rename 500 Internal Server => Internal

--- boilerplates/react/files/pages/_error/+Page.tsx
+++ boilerplates/react/files/pages/_error/+Page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
   }
   return (
     <>
-      <h1>500 Internal Server Error</h1>
+      <h1>Internal Error</h1>
       <p>Something went wrong.</p>
     </>
   );
