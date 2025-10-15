c54d31e52d10103342d301ea38ec28d2cbb2b185
Joël Charles <joel.charles91@gmail.com>
2025-10-15 18:41:35 +0200

    fix: disable preview mode for Vercel

--- boilerplates/vercel/files/$package.json.ts
+++ boilerplates/vercel/files/$package.json.ts
@@ -3,11 +3,10 @@ import { loadPackageJson, type TransformerProps } from "@batijs/core";
 export default async function getPackageJson(props: TransformerProps) {
   const packageJson = await loadPackageJson(props, await import("../package.json").then((x) => x.default));
 
-  return packageJson
-    .setScript("prod", {
-      value: "vike build && node dist/server/index.mjs",
-      precedence: 25,
-      warnIfReplaced: true,
-    })
-    .addDependencies(["@photonjs/vercel"]);
+  return (
+    packageJson
+      // vite preview does not make sense when targetting Vercel
+      .removeScript("prod")
+      .addDependencies(["@photonjs/vercel"])
+  );
 }
--- packages/tests/tests/FRAMEWORK+vercel.spec.ts
+++ packages/tests/tests/FRAMEWORK+vercel.spec.ts
@@ -12,18 +12,4 @@ await describeMultipleBati([
         expect(await res.text()).not.toContain('{"is404":true}');
       });
     }),
-  // preview
-  () =>
-    describeBati(
-      ({ test, expect, fetch }) => {
-        test("home", async () => {
-          const res = await fetch("/");
-          expect(res.status).toBe(200);
-          expect(await res.text()).not.toContain('{"is404":true}');
-        });
-      },
-      {
-        mode: "prod",
-      },
-    ),
 ]);
