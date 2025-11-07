45ac1ea7566752cb4a901b69312ce9aa66da0ff9
Romuald Brillout <git@brillout.com>
2025-11-07 14:54:43 +0100

    wip

--- boilerplates/react/files/$README.md.ts
+++ boilerplates/react/files/$README.md.ts
@@ -10,14 +10,14 @@ This app is ready to start. It's powered by [Vike](https://vike.dev) and [React]
 
 ### Plus files
 
-[The \`+\` files are the interface](https://vike.dev/config) between Vike and your code.
+[The + files are the interface](https://vike.dev/config) between Vike and your code.
 
 - [\`+config.ts\`](https://vike.dev/settings) — Settings (e.g. global default \`<title>\`)
 - [\`+Page.tsx\`](https://vike.dev/Page) — The \`<Page>\` component
-- [\`+data.ts\`](https://vike.dev/data) — Fetching data (consumed by your \`<Page>\` component)
-- [\`+Layout.tsx\`](https://vike.dev/Layout) — The \`<Layout>\` component (wrapping your \`<Page>\` components)
-- [\`/pages/_error/+Page.tsx\`](https://vike.dev/error-page) — The [error page]() rendered when an error occurs
-- [\`+onPageTransitionStart.ts\`](https://vike.dev/onPageTransitionStart) and \`+onPageTransitionEnd.ts\` — Implementing page transition animations
+- [\`+data.ts\`](https://vike.dev/data) — Fetching data (for your \`<Page>\` component)
+- [\`+Layout.tsx\`](https://vike.dev/Layout) — The \`<Layout>\` component (wraps your \`<Page>\` components)
+- [\`/pages/_error/+Page.tsx\`](https://vike.dev/error-page) — The error page (rendered when an error occurs)
+- [\`+onPageTransitionStart.ts\`](https://vike.dev/onPageTransitionStart) and \`+onPageTransitionEnd.ts\` — For page transition animations
 - [\`+Head.tsx\`](https://vike.dev/Head) - Sets \`<head>\` tags
 
 ### Routing
