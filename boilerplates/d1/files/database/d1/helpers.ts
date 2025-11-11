494e210d45ed94bdef72e92610549a1aad4c88d9
Joël Charles <joel.charles91@gmail.com>
2025-11-11 20:22:17 +0100

    fix: upgrade deps (#607)

--- boilerplates/d1/files/database/d1/helpers.ts
+++ boilerplates/d1/files/database/d1/helpers.ts
@@ -5,8 +5,14 @@ import type { RuntimeAdapter } from "@universal-middleware/core";
  * Retrieve Cloudflare `env.DB` from `universal-middleware` runtime
  */
 export async function getDbFromRuntime(runtime: RuntimeAdapter): Promise<D1Database> {
-  if (runtime.runtime === "workerd" && runtime.env) {
-    return runtime.env.DB as D1Database;
+  if (BATI.has("h3")) {
+    if (runtime.runtime === "workerd" && runtime.adapter === "h3") {
+      return runtime.h3.context.DB as D1Database;
+    }
+  } else {
+    if (runtime.runtime === "workerd" && runtime.env) {
+      return runtime.env.DB as D1Database;
+    }
   }
 
   throw new Error("Database is not available");
