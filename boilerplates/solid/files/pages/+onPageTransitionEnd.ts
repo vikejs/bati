5e7733c6bc3ec04a4461fcd184b02c7591236f59
Romuald Brillout <git@brillout.com>
2025-11-11 19:29:43 +0100

    minor refactor: rename Is backwards navigation? => pageContext.isBackwardNavigation

--- boilerplates/solid/files/pages/+onPageTransitionEnd.ts
+++ boilerplates/solid/files/pages/+onPageTransitionEnd.ts
@@ -2,6 +2,6 @@ import type { PageContextClient } from "vike/types";
 
 export async function onPageTransitionEnd(pageContext: PageContextClient) {
   console.log("Page transition end");
-  console.log("Is backwards navigation?", pageContext.isBackwardNavigation);
+  console.log("pageContext.isBackwardNavigation", pageContext.isBackwardNavigation);
   document.body.classList.remove("page-transition");
 }
