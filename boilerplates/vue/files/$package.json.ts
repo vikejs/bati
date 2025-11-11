251397c4797f7d8e7fe7a2b20dcb84daba68b653
Romuald Brillout <git@brillout.com>
2025-11-11 18:39:06 +0100

    remove markdown

--- boilerplates/vue/files/$package.json.ts
+++ boilerplates/vue/files/$package.json.ts
@@ -5,14 +5,6 @@ export default async function getPackageJson(props: TransformerProps) {
 
   return packageJson
     .addDevDependencies(["vite"])
-    .addDependencies([
-      "@vitejs/plugin-vue",
-      "@vue/compiler-sfc",
-      "@vue/server-renderer",
-      "unplugin-vue-markdown",
-      "vike-vue",
-      "vike",
-      "vue",
-    ])
+    .addDependencies(["@vitejs/plugin-vue", "@vue/compiler-sfc", "@vue/server-renderer", "vike-vue", "vike", "vue"])
     .addDependencies(["vue-gtag"], props.meta.BATI.has("google-analytics"));
 }
--- boilerplates/vue/files/$vite.config.ts.ts
+++ boilerplates/vue/files/$vite.config.ts.ts
@@ -6,14 +6,6 @@ export default async function getViteConfig(props: TransformerProps) {
   addVitePlugin(mod, {
     from: "@vitejs/plugin-vue",
     constructor: "vue",
-    options: {
-      include: [/\.vue$/, /\.md$/],
-    },
-  });
-  addVitePlugin(mod, {
-    from: "unplugin-vue-markdown/vite",
-    constructor: "md",
-    options: {},
   });
 
   return mod.generate().code;
