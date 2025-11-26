d60503eb8a3c07d710ff2bc5a3d584681c04cb38
Romuald Brillout <git@brillout.com>
2025-11-26 21:59:23 +0100

    fix: revert './worker-configuration.d.ts'

--- boilerplates/cloudflare/files/$tsconfig.json.ts
+++ boilerplates/cloudflare/files/$tsconfig.json.ts
@@ -3,7 +3,9 @@ import { loadAsJson, type TransformerProps } from "@batijs/core";
 export default async function getTsConfig(props: TransformerProps) {
   const tsConfig = await loadAsJson(props);
 
+  /* TO-DO/eventually: how to avoid typecheck CI fail?
   tsConfig.compilerOptions.types = [...(tsConfig.compilerOptions.types ?? []), "./worker-configuration.d.ts"];
+  */
 
   return tsConfig;
 }
