5a1c76f3b6f2bfde0c6ce3ee4278e0e470f724c1
Romuald Brillout <git@brillout.com>
2025-11-27 11:52:39 +0100

    try

--- boilerplates/cloudflare/files/$tsconfig.json.ts
+++ boilerplates/cloudflare/files/$tsconfig.json.ts
@@ -3,9 +3,7 @@ import { loadAsJson, type TransformerProps } from "@batijs/core";
 export default async function getTsConfig(props: TransformerProps) {
   const tsConfig = await loadAsJson(props);
 
-  /* TO-DO/eventually: how to avoid typecheck CI fail?
   tsConfig.compilerOptions.types = [...(tsConfig.compilerOptions.types ?? []), "./worker-configuration.d.ts"];
-  */
 
   return tsConfig;
 }
