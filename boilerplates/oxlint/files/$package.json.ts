3d951ed1c5d5ae7c492dabf9d0097648d80fb17e
Rom <git@brillout.com>
2025-12-02 10:45:54 +0100

    Add oxlint `--ignore-path` iff. CI or `--skip-git` (#636) (#641)

--- boilerplates/oxlint/files/$package.json.ts
+++ boilerplates/oxlint/files/$package.json.ts
@@ -3,9 +3,11 @@ import { loadPackageJson, type TransformerProps } from "@batijs/core";
 export default async function getPackageJson(props: TransformerProps) {
   const packageJson = await loadPackageJson(props, await import("../package.json").then((x) => x.default));
 
+  const gitIgnore = props.meta.BATI_IS_CI || props.meta.BATI_SKIP_GIT ? "--ignore-path .gitignore " : "";
+  const CWD = ".";
   return packageJson
     .setScript("lint", {
-      value: "oxlint --type-aware --ignore-path .gitignore .",
+      value: `oxlint --type-aware ${gitIgnore}${CWD}`,
       precedence: 0,
     })
     .addDevDependencies(["oxlint", "oxlint-tsgolint"]);
