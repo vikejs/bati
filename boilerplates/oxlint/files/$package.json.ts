cb1dd4b9b64e0647aa67d515f11ce1902b43db85
Romuald Brillout <git@brillout.com>
2025-12-02 10:32:18 +0100

    minor refactor

--- boilerplates/oxlint/files/$package.json.ts
+++ boilerplates/oxlint/files/$package.json.ts
@@ -4,9 +4,10 @@ export default async function getPackageJson(props: TransformerProps) {
   const packageJson = await loadPackageJson(props, await import("../package.json").then((x) => x.default));
 
   const gitIgnore = props.meta.BATI_IS_CI || props.meta.BATI_SKIP_GIT ? "--ignore-path .gitignore " : "";
+  const CWD = ".";
   return packageJson
     .setScript("lint", {
-      value: `oxlint --type-aware ${gitIgnore}.`,
+      value: `oxlint --type-aware ${gitIgnore}${CWD}`,
       precedence: 0,
     })
     .addDevDependencies(["oxlint", "oxlint-tsgolint"]);
