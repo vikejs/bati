a3e7e0c380c04c1ef7d48c06e6ef54e910289916
Romuald Brillout <git@brillout.com>
2025-12-02 10:21:28 +0100

    update to-do

--- boilerplates/oxlint/files/$package.json.ts
+++ boilerplates/oxlint/files/$package.json.ts
@@ -3,6 +3,7 @@ import { loadPackageJson, type TransformerProps } from "@batijs/core";
 export default async function getPackageJson(props: TransformerProps) {
   const packageJson = await loadPackageJson(props, await import("../package.json").then((x) => x.default));
 
+  // TODO/copilot: add `--ignore-path .gitignore` only if CI or `--skip-git`, see https://github.com/vikejs/bati/pull/636#discussion_r2577191441
   return packageJson
     .setScript("lint", {
       value: "oxlint --type-aware --ignore-path .gitignore .",
