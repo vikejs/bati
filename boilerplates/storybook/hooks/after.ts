e5f585cf20838154868f1998d0c84fc97690be68
Joël Charles <joel.charles91@gmail.com>
2026-03-12 15:40:23 +0100

    fix(storybook): skip installation

--- boilerplates/storybook/hooks/after.ts
+++ boilerplates/storybook/hooks/after.ts
@@ -5,6 +5,6 @@ import { packageManager } from "@batijs/core";
 export default async function onafter(cwd: string, meta: VikeMeta) {
   const isInteractive = !meta.BATI_IS_CI && !meta.BATI_TEST;
   const pm = packageManager();
-  const command = `${pm.exec} storybook@latest init --no-dev${isInteractive ? "" : " --yes"}`;
+  const command = `${pm.exec} storybook@latest init --skip-install --no-dev${isInteractive ? "" : " --yes"}`;
   execSync(command, { cwd, stdio: "inherit" });
 }
