d131f1717332e2bc20727a35d0379460194bae89
Joël Charles <joel.charles91@gmail.com>
2026-03-24 17:57:14 +0100

    chore: cleanup

--- packages/compile/index.ts
+++ packages/compile/index.ts
@@ -2,11 +2,6 @@ import { clean } from "./clean.js";
 import { copyFilesToDist } from "./copy.js";
 import { build } from "./tsdown.js";
 
-// import { buildTypes } from "./dts.js";
-// import { build, buildConfig } from "./esbuild.js";
-
 await clean();
-// await buildConfig();
-// await build();
 await build();
 await copyFilesToDist();
