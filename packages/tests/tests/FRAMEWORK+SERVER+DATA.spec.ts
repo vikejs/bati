c6b1c947fc984080b5d6da5895d8d37ae45186a9
Joel Charles <joel.charles91@gmail.com>
2026-04-03 15:09:23 +0200

    chore

--- packages/tests/tests/FRAMEWORK+SERVER+DATA.spec.ts
+++ packages/tests/tests/FRAMEWORK+SERVER+DATA.spec.ts
@@ -144,17 +144,17 @@ await describeMultipleBati([
       });
     }),
   // preview
-  () =>
-    describeBati(
-      ({ test, expect, fetch }) => {
-        test("home", async () => {
-          const res = await fetch("/");
-          expect(res.status).toBe(200);
-          expect(await res.text()).not.toContain('{"is404":true}');
-        });
-      },
-      {
-        mode: "prod",
-      },
-    ),
+  // () =>
+  //   describeBati(
+  //     ({ test, expect, fetch }) => {
+  //       test("home", async () => {
+  //         const res = await fetch("/");
+  //         expect(res.status).toBe(200);
+  //         expect(await res.text()).not.toContain('{"is404":true}');
+  //       });
+  //     },
+  //     {
+  //       mode: "prod",
+  //     },
+  //   ),
 ]);
