b0b25d2f069fdde0c5457b34049b4b8d45f43601
Joel Charles <joel.charles91@gmail.com>
2026-04-13 15:27:22 +0200

    feat: remove manual glue plugin

--- boilerplates/netlify/files/$package.json.ts
+++ boilerplates/netlify/files/$package.json.ts
@@ -3,5 +3,5 @@ import { loadPackageJson, type TransformerProps } from "@batijs/core";
 export default async function getPackageJson(props: TransformerProps): Promise<unknown> {
   const packageJson = await loadPackageJson(props, await import("../package.json").then((x) => x.default));
 
-  return packageJson.addDevDependencies(["@netlify/vite-plugin", "@universal-deploy/netlify"]);
+  return packageJson.addDevDependencies(["@netlify/vite-plugin"]);
 }
