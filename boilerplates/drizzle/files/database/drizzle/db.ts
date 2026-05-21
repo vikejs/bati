ae84bb94062f730f47ec9f60c4e7d13483c1fd36
Joel Charles <joel.charles91@gmail.com>
2026-05-21 15:23:23 +0200

    fix: drizzle db migration at runtime

--- boilerplates/drizzle/files/database/drizzle/db.ts
+++ boilerplates/drizzle/files/database/drizzle/db.ts
@@ -1,11 +1,16 @@
 import Database from "better-sqlite3";
 import { drizzle as drizzleSqlite } from "drizzle-orm/better-sqlite3";
+import { migrate as migrateSqlite } from "drizzle-orm/better-sqlite3/migrator";
 import { drizzle as drizzleD1 } from "drizzle-orm/d1";
 
 //# !BATI.hasD1
 export function dbSqlite() {
   const sqlite = new Database(process.env.DATABASE_URL);
-  return drizzleSqlite(sqlite);
+  const db = drizzleSqlite(sqlite);
+  migrateSqlite(db, {
+    migrationsFolder: "./database/migrations",
+  });
+  return db;
 }
 
 //# BATI.hasD1
