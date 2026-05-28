d2bd30e7825e46aebf9a79a43c0e90d529e0edec
Joel Charles <joel.charles91@gmail.com>
2026-05-28 12:18:37 +0200

    feat

---
+++ boilerplates/docker-compose/files/$package.json.ts
@@ -0,0 +1,11 @@
+import { loadPackageJson, type TransformerProps } from "@batijs/core";
+
+export default async function getPackageJson(props: TransformerProps): Promise<unknown> {
+  const packageJson = await loadPackageJson(props, await import("../package.json").then((x) => x.default));
+
+  return packageJson.setScript("prod", {
+    value: "docker compose up -d --build",
+    precedence: 25,
+    warnIfReplaced: true,
+  });
+}
